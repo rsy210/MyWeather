af8716b92cdbbd1b98c8933afeadaf3e5875cb21
RSY <626929968@qq.com>
2015-06-04 13:45:35 +0800

    weather apk
    based on android

---
+++ MyWeather/MyWeather/src/edu/hrbeu/myweather/EncodeUtil.java
@@ -0,0 +1,134 @@
+package edu.hrbeu.myweather;
+
+import javax.crypto.Mac;
+
+import java.net.URLEncoder;
+import java.security.InvalidKeyException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncodeUtil {
+
+	public String url;
+	private static final char last2byte = (char) Integer
+			.parseInt("00000011", 2);
+	private static final char last4byte = (char) Integer
+			.parseInt("00001111", 2);
+	private static final char last6byte = (char) Integer
+			.parseInt("00111111", 2);
+	private static final char lead6byte = (char) Integer
+			.parseInt("11111100", 2);
+	private static final char lead4byte = (char) Integer
+			.parseInt("11110000", 2);
+	private static final char lead2byte = (char) Integer
+			.parseInt("11000000", 2);
+	private static final char[] encodeTable = new char[] { 'A', 'B', 'C', 'D',
+			'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+			'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+			'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+			'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+			'4', '5', '6', '7', '8', '9', '+', '/' };
+
+	public static String standardURLEncoder(String data, String key) {
+		byte[] byteHMAC = null;
+		String urlEncoder = "";
+		try {
+			Mac mac = Mac.getInstance("HmacSHA1");
+			SecretKeySpec spec = new SecretKeySpec(key.getBytes(), "HmacSHA1");
+			mac.init(spec);
+			byteHMAC = mac.doFinal(data.getBytes());
+			if (byteHMAC != null) {
+				String oauth = encode(byteHMAC);
+				if (oauth != null) {
+					urlEncoder = URLEncoder.encode(oauth, "utf8");
+				}
+			}
+		} catch (InvalidKeyException e1) {
+			e1.printStackTrace();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+		return urlEncoder;
+	}
+
+	public static String encode(byte[] from) {
+		StringBuffer to = new StringBuffer((int) (from.length * 1.34) + 3);
+		int num = 0;
+		char currentByte = 0;
+		for (int i = 0; i < from.length; i++) {
+			num = num % 8;
+			while (num < 8) {
+				switch (num) {
+				case 0:
+					currentByte = (char) (from[i] & lead6byte);
+					currentByte = (char) (currentByte >>> 2);
+					break;
+				case 2:
+					currentByte = (char) (from[i] & last6byte);
+					break;
+				case 4:
+					currentByte = (char) (from[i] & last4byte);
+					currentByte = (char) (currentByte << 2);
+					if ((i + 1) < from.length) {
+						currentByte |= (from[i + 1] & lead2byte) >>> 6;
+					}
+					break;
+				case 6:
+					currentByte = (char) (from[i] & last2byte);
+					currentByte = (char) (currentByte << 4);
+					if ((i + 1) < from.length) {
+						currentByte |= (from[i + 1] & lead4byte) >>> 4;
+					}
+					break;
+				}
+				to.append(encodeTable[currentByte]);
+				num += 6;
+			}
+		}
+		if (to.length() % 4 != 0) {
+			for (int i = 4 - to.length() % 4; i > 0; i--) {
+				to.append("=");
+			}
+		}
+		return to.toString();
+	}
+
+	public EncodeUtil() {
+		try {
+			String areaid = "101010100";
+			String type = "forecast_v";
+			String appid = "c2ffc8e63c5b40ca";
+			String appid_six = "c2ffc8";
+			String key = "0244f8_SmartWeatherAPI_5e9551e";
+			
+			Date dt=new Date();//如果不需要格式,可直接用dt,dt就是当前系统时间
+			DateFormat df = new SimpleDateFormat("yyyyMMddHHmm");//设置显示格式
+			String nowTime="";
+			nowTime= df.format(dt);//用DateFormat的format()方法在dt中获取并以yyyy/MM/dd HH:mm:ss格式显示
+			System.out.println("nowTime:"+nowTime);
+			
+			
+			// 需要加密的数据
+			String data = "http://open.weather.com.cn/data/?areaid="+areaid+"&type="+type+"&date="+nowTime+"&appid="+appid;
+			// 密钥
+		//	String key = "0244f8_SmartWeatherAPI_5e9551e";
+
+			String publicKey = standardURLEncoder(data, key);
+//			
+//			String data2 = "http://open.weather.com.cn/data/?areaid="+areaid+"&type="+type+"&date="+nowTime+"&appid="+appid_six;
+//			
+//			System.out.println(str);
+			url="http://open.weather.com.cn/data/?areaid="+areaid+"&type="+type+"&date="+nowTime+"&appid="+appid_six+"&key="+publicKey;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	// AppId：c2ffc8e63c5b40ca
+	// Private_Key：0244f8_SmartWeatherAPI_5e9551e
+//	lWTMWq3QRJyLstvvvv6So0DBkQ0%3D
+//	hPZf1%2F5Ag%2F4JS2A%2BcYmpj5Yv2YA%3D
+}
---
+++ MyWeather/MyWeather/src/edu/hrbeu/myweather/Weather.java
@@ -0,0 +1,27 @@
+package edu.hrbeu.myweather;
+
+/**
+ * Created by ice on 2014/11/7.
+ */
+public class Weather {
+
+	public String city;// c3
+	public String province;// c7
+	public String date;// f0
+
+	
+	public String[] weatherD = new String[3];// fa
+	public String[] weatherN = new String[3];// fb
+	
+	public String[] temperatureD = new String[3];// fc
+	public String[] temperatureN = new String[3];// fd
+	public String[] humidity = new String[3];
+	public String[] windDD = new String[3];// fe
+	public String[] windDN = new String[3];// ff
+
+	public String[] windPD = new String[3];// fg
+
+	public String[] windPN = new String[3];// fh
+
+
+}
