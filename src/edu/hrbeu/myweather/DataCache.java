557c3a8acbb2f51df7fab54bf31fc69dcde083ef
rsy <626929968@qq.com>
2015-06-16 12:01:16 +0800

    add data cache

---
+++ src/edu/hrbeu/myweather/DataCache.java
@@ -0,0 +1,114 @@
+package edu.hrbeu.myweather;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataCache {
+
+	static final String KEY_ROWID = "_id";
+	static final String KEY_NAME = "name";
+	static final String KEY_EMAIL = "email";
+	static final String TAG = "DBAdapter";
+	
+	static final String DATABASE_NAME = "MyDB";
+	static final String DATABASE_TABLE = "contacts";
+	static final int DATABASE_VERSION = 1;
+	
+	static final String DATABASE_CREATE = 
+			"create table contacts( _id integer primary key autoincrement, " + 
+			"name text not null, email text not null);";
+	private Context context;
+	
+	DatabaseHelper DBHelper;
+	SQLiteDatabase db;
+	
+	public void DBAdapter(Context cxt)
+	{
+		this.context = cxt;
+		DBHelper = new DatabaseHelper(context);
+	}
+	
+	private static class DatabaseHelper extends SQLiteOpenHelper
+	{
+
+		DatabaseHelper(Context context)
+		{
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			try
+			{
+				db.execSQL(DATABASE_CREATE);
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+			Log.wtf(TAG, "Upgrading database from version "+ oldVersion + "to "+
+			 newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS contacts");
+			onCreate(db);
+		}
+	}
+	
+	//open the database
+	public DataCache open() throws SQLException
+	{
+		db = DBHelper.getWritableDatabase();
+		return this;
+	}
+	//close the database
+	public void close()
+	{
+		DBHelper.close();
+	}
+	
+	//insert a contact into the database
+	public long insertContact(String name, String email)
+	{
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_NAME, name);
+		initialValues.put(KEY_EMAIL, email);
+		return db.insert(DATABASE_TABLE, null, initialValues);
+	}
+	//delete a particular contact
+	public boolean deleteContact(long rowId)
+	{
+		return db.delete(DATABASE_TABLE, KEY_ROWID + "=" +rowId, null) > 0;
+	}
+	//retreves all the contacts
+	public Cursor getAllContacts()
+	{
+		return db.query(DATABASE_TABLE, new String[]{ KEY_ROWID,KEY_NAME,KEY_EMAIL}, null, null, null, null, null);
+	}
+	//retreves a particular contact
+	public Cursor getContact(long rowId) throws SQLException
+	{
+		Cursor mCursor = 
+				db.query(true, DATABASE_TABLE, new String[]{ KEY_ROWID,
+						 KEY_NAME, KEY_EMAIL}, KEY_ROWID + "=" + rowId, null, null, null, null, null);
+		if (mCursor != null)
+			mCursor.moveToFirst();
+		return mCursor;
+	}
+	//updates a contact
+	public boolean updateContact(long rowId, String name, String email)
+	{
+		ContentValues args = new ContentValues();
+		args.put(KEY_NAME, name);
+		args.put(KEY_EMAIL, email);
+		return db.update(DATABASE_TABLE, args, KEY_ROWID + "=" +rowId, null) > 0;
+	}
+}
